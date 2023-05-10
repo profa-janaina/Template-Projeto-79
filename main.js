64321eb7f04ff4563871ce0d1798f523f1e805f7
profa-janaina <111704392+profa-janaina@users.noreply.github.com>
2023-05-10 17:18:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
 menuListArray = ["Pizza Vegetariana",
-"Pizza de Frango",
-"Pizza Portuguesa",
-"Pizza Quatro Queijos",
-"Pizza de Calabresa",
-"Pizza Extravaganza"];
+                 //adicionar mais itens
+                ];
 
 function addItem(){
 var htmldata;
-var imgtags='<img id="im1" src="images/pizzaImg.png"/>'  // FALTA ESTA LINHA
+var imgtags='<img id="im1" src="images/pizzaImg.png"/>'  
 var item=document.getElementById("addItem").value;
-menuListArray.sort();
-htmldata=""
-for(var i=0;i<menuListArray.length;i++){
-htmldata=htmldata+imgtags+ menuListArray[i]+'<br>'; // corrigir esta linha
+  
+//completar o código
+  
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
 }
-document.getElementById("displayAddedMenu").innerHTML = htmldata; // corrigir esta linha
 
-}
 function getMenu(){
 var htmldata="";
-menuListArray.sort();
-for(var i=0;i<menuListArray.length;i++){
-htmldata=htmldata+ menuListArray[i] + '<br>'
-}
+  
+//completar o código
+  
 document.getElementById("displayMenu").innerHTML = htmldata;
 }
 
 function addTop(){
-var item=document.getElementById("addItem").value;
-menuListArray.push(item);
+//completar o código
+  
 document.getElementById("addItem").value="";
 addItem();
 }
